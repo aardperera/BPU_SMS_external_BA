fbaf066883bf5751958d3b1c395d03118313daeb
Ranjana Perera <76731513+aardperera@users.noreply.github.com>
2024-02-20 08:49:07 +0530

    add link to detailed result sheet with repeat result page

--- reportResultRelated.php
+++ reportResultRelated.php
@@ -15,16 +15,16 @@ $db = new DBOperations();
 <form method="post" onsubmit="return false;">
   <table class="searchResults">
   
-  <tr><td><h2><a href="resultsSheetViewMain.php">Detailed Result Sheet without withheld Results</a></h2></td></tr>
-  <tr><td><h2><a href="resultsSheetViewMain2.php">Detailed Result Sheet with withheld Results</a></h2></td></tr>
-  <tr><td><h2><a href="repeatResultView.php">Repeat Result Sheet</a></h2></td></tr>  
-   <tr><td><h2><a href="statusReport.php">Student Degree Status Report</a></h2></td></tr> 
-   <tr><td><h2><a href="ResultViewNew.php">Repeat Subject of  Incompleted Students </a></h2></td></tr> 
+    <tr><td><h2><a href="resultsSheetViewMain.php">Detailed Result Sheet without withheld Results</a></h2></td></tr>
+    <tr><td><h2><a href="resultsSheetViewMain2.php">Detailed Result Sheet with withheld Results</a></h2></td></tr>
+    <tr><td><h2><a href="repeatResultView.php">Repeat Result Sheet</a></h2></td></tr>
+    <tr><td><h2><a href="rsWithRepeat.php">Detailed Result Sheet with Repeat Results</a></h2></td></tr>
+    <tr><td><h2><a href="statusReport.php">Student Degree Status Report</a></h2></td></tr> 
+    <tr><td><h2><a href="ResultViewNew.php">Repeat Subject of  Incompleted Students </a></h2></td></tr> 
     <tr><td><h2><a href="allAbsent.php">All Absent Student Report</a></h2></td></tr> 
-     <tr><td><h2><a href="subPassFailAb.php">Pass/Fail/Absent/Withheld Student Details Subjectwise</a></h2></td></tr> 
-      
-     <tr><td><h2><a href="completeRe.php">Examination Subject Results Summary</a></h2></td></tr>
-	 <tr><td><h2><a href="transcript3Main.php">Examination Student Transcript </a></h2></td></tr>
+    <tr><td><h2><a href="subPassFailAb.php">Pass/Fail/Absent/Withheld Student Details Subjectwise</a></h2></td></tr> 
+    <tr><td><h2><a href="completeRe.php">Examination Subject Results Summary</a></h2></td></tr>
+    <tr><td><h2><a href="transcript3Main.php">Examination Student Transcript </a></h2></td></tr>
   </table>
 </form>
 
